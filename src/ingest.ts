977947ade2275ebfab0a53442e20b0699174c13d
Ranjeet <kiranjeetkr80@gmail.com>
2025-07-20 19:29:10 +0530

    ingest

--- src/ingest.ts
+++ src/ingest.ts
@@ -1,12 +1,27 @@
 import fs from "fs";
 import pdfParse from "pdf-parse";
+import {RecursiveCharacterTextSplitter} from "langchain/text_splitter";
+import {MemoryVectorStore} from "langchain/vectorstores/memory";
+import {OpenAIEmbeddings} from "@langchain/openai";
+export const loadPdf = async (path: string): Promise<string> => {
+  const dataBuffer = fs.readFileSync(path);
+  const pdfData = await pdfParse(dataBuffer);
+  return pdfData.text;
+};
 
-export const loadPdf = async (path:string) : Promise<string> =>{
-    const dataBuffer = fs.readFileSync(path);
+export const embedPdf = async () =>{
+    const text = await loadPdf("data/Resume_web.pdf");
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize:1000,
+        chunkOverlap:100
+    });
 
-    const pdfData = await pdfParse(dataBuffer);
-    console.log(pdfData);
+    const chunks =  await splitter.createDocuments([text]);
 
-    return "Dafa";
+    //genrate embeddings and store in vector DB
+    const vectorstore = await MemoryVectorStore.fromDocuments(
+        chunks,
+        new OpenAIEmbeddings()
+    )
+    return vectorstore;
 }
-loadPdf("data/Resume_web.pdf");
